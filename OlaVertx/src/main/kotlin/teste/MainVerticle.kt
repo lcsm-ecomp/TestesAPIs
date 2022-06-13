429a96511a86669799e577410920d0d19bb6d53c
lcsm-ecomp <32273415+lcsm-ecomp@users.noreply.github.com>
2022-06-13 19:26:14 +0000

    Teste de Vetx

--- OlaVertx/src/main/kotlin/teste/MainVerticle.kt
+++ OlaVertx/src/main/kotlin/teste/MainVerticle.kt
@@ -2,24 +2,28 @@ package teste
 
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Promise
+import io.vertx.ext.web.Router
 
 class MainVerticle : AbstractVerticle() {
 
-  override fun start(startPromise: Promise<Void>) {
-    vertx
-      .createHttpServer()
-      .requestHandler { req ->
+  override fun start() {
+    val server = vertx.createHttpServer()
+
+    val router = Router.router(vertx)
+
+    router.route("/ola.txt").handler {
+        it.response().end("ola para todos")
+    }
+    
+    
+    router.route().handler { req ->
         req.response()
           .putHeader("content-type", "text/plain")
           .end("Hello from Vert.x!")
       }
-      .listen(8888) { http ->
-        if (http.succeeded()) {
-          startPromise.complete()
-          println("HTTP server started on port 8888")
-        } else {
-          startPromise.fail(http.cause());
-        }
-      }
+    
+    server.requestHandler(router)  
+      .listen(8888)
+      
   }
 }
